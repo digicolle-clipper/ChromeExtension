69c54ea476a9016c36065f6852d3a3f2be86b8e9
niryuu <niryuu@gmail.com>
2015-11-28 21:54:45 +0900

    fully refactored and work fine on retina

--- src/digicolle-clipper/overlay.js
+++ src/digicolle-clipper/overlay.js
@@ -1,41 +1,39 @@
-var overlay = document.createElement('div');
-var state = 'start';
-var x, y, w, h;
-overlay.setAttribute('id', 'overlay');
-overlay.setAttribute('style', 'width: 100%;height: 100%;position: absolute;top: 0;left: 0;background-color: #000000;opacity: 0.5;')
-document.body.appendChild(overlay);
-overlay.addEventListener('click', function(evt) {
-  switch(state) {
-    case 'start':
-      x = evt.clientX;
-      y = evt.clientY;
-      state = 'clickedonce';
-      break;
-    case 'clickedonce':
-      w = evt.clientX - x;
-      h = evt.clientY - y;
-      console.log([x,y,w,h]);
-      chrome.runtime.sendMessage([x,y,w,h], function(response) {
-        console.log(response);
-      });
-      break;
-    default:
-      break;
-  }
-});
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-        sendResponse('hoge');
-        console.log(request);
+  sendResponse('ok');
+  var overlay = document.createElement('div');
+  var state = 'start';
+  var x, y, w, h;
+  overlay.setAttribute('id', 'overlay');
+  //overlay.setAttribute('style', 'width: 100%;height: 100%;position: absolute;top: 0;left: 0;background-color: #000000;opacity: 0.5;')
+  document.body.appendChild(overlay);
+  overlay.addEventListener('click', function(evt) {
+    switch(state) {
+      case 'start':
+        console.log(evt);
+        x = evt.clientX * window.devicePixelRatio;
+        y = evt.clientY * window.devicePixelRatio;
+        state = 'clickedonce';
+        break;
+      case 'clickedonce':
+        w = (evt.clientX * window.devicePixelRatio) - x;
+        h = (evt.clientY * window.devicePixelRatio) - y;
+        console.log([x,y,w,h]);
         var img = document.createElement('img');
         img.setAttribute('src', request);
-        img.style.visiblity = 'hidden';
+        //img.style.visiblity = 'hidden';
 
         setTimeout(function() {
           var canvas = document.createElement('canvas');
+          canvas.setAttribute('width', w + 'px');
+          canvas.setAttribute('height', h + 'px');
           var context = canvas.getContext('2d');
           context.fillRect(0, 0, w, h);
           context.drawImage(img, -x, -y);
-          document.body.appendChild(canvas);
           document.body.removeChild(document.getElementById('overlay'));
         }, 100);
+        break;
+      default:
+        break;
+    }
+  });
 });
--- src/digicolle-clipper/popup.js
+++ src/digicolle-clipper/popup.js
@@ -1,19 +1,8 @@
 chrome.browserAction.onClicked.addListener(function(tab) {
   chrome.tabs.executeScript(null, {file: 'overlay.js'});
-});
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-  sendResponse('hoge');
-  capture(request);
-});
-function capture(rect) {
-  console.log(rect);
   chrome.windows.getCurrent(function (win) {    
     chrome.tabs.captureVisibleTab(win.id,{"format":"png"}, function(imgUrl) {
-      chrome.tabs.query(
-        { currentWindow: true, active: true },
-        function (tabArray) { chrome.tabs.sendMessage(tabArray[0].id, imgUrl, function(response) {
-        }); }
-      );
+      chrome.tabs.sendMessage(tab.id, imgUrl, function(response) {});
     });
-  }); 
-}
+  });
+});
